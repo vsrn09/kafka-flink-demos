ed8fbfe921af91a3874ed488be19234821520d33
Venkat Namala <56455357+vsrn09@users.noreply.github.com>
2024-08-20 11:14:14 -0500

    Create CDCProducerApplication.java

---
+++ cdc-producer/CDCProducerApplication.java
@@ -0,0 +1,12 @@
+package com.yourcompany.retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdcProducerApplication.class, args);
+    }
+}
