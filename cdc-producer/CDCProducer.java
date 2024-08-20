1e8cbd5b917f2b9138c8e661ba26817bacb5042e
Venkat Namala <56455357+vsrn09@users.noreply.github.com>
2024-08-20 11:13:32 -0500

    Update CDCProducer.java

--- cdc-producer/CDCProducer.java
+++ cdc-producer/CDCProducer.java
@@ -1,73 +1,55 @@
+package com.yourcompany.retail;
+
+import com.github.javafaker.Faker;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.Schema;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
-import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
-import com.github.javafaker.Faker;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
-import java.util.Properties;
+import javax.annotation.PostConstruct;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class CDCProducer {
+@Service
+public class CdcProducer {
 
+    private final KafkaTemplate<String, GenericRecord> kafkaTemplate;
+    private final Faker faker;
+    private final Random random;
+    private static final String TOPIC = "cdc_retail_member";
     private static final String SCHEMA_STRING = "{\n" +
             "  \"fields\": [\n" +
             "    {\n" +
             "      \"name\": \"member_id\",\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"arg.properties\": {\n" +
-            "        \"regex\": \"[A-Za-z0-9]+\"\n" +
-            "      },\n" +
-            "      \"doc\": \"Unique identifier for the retail member\"\n" +
+            "      \"type\": \"string\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"name\": \"name\",\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"doc\": \"Full name of the retail member\"\n" +
+            "      \"type\": \"string\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"name\": \"email\",\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"arg.properties\": {\n" +
-            "        \"regex\": \"^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$\"\n" +
-            "      },\n" +
-            "      \"doc\": \"Email address of the retail member\"\n" +
+            "      \"type\": \"string\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"name\": \"phone\",\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"doc\": \"Phone number of the retail member\"\n" +
+            "      \"type\": \"string\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"name\": \"address\",\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"doc\": \"Mailing address of the retail member\"\n" +
+            "      \"type\": \"string\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"name\": \"operation\",\n" +
-            "      \"type\": \"string\",\n" +
-            "      \"arg.properties\": {\n" +
-            "        \"options\": [\n" +
-            "          \"INSERT\",\n" +
-            "          \"UPDATE\",\n" +
-            "          \"DELETE\"\n" +
-            "        ]\n" +
-            "      }\n" +
+            "      \"type\": \"string\"\n" +
             "    },\n" +
             "    {\n" +
             "      \"name\": \"ts\",\n" +
             "      \"type\": {\n" +
             "        \"type\": \"long\",\n" +
             "        \"logicalType\": \"timestamp-millis\"\n" +
-            "      },\n" +
-            "      \"doc\": \"Timestamp of the change event\"\n" +
+            "      }\n" +
             "    }\n" +
             "  ],\n" +
             "  \"name\": \"CdcRetailMember\",\n" +
@@ -75,29 +57,16 @@ public class CDCProducer {
             "  \"type\": \"record\"\n" +
             "}";
 
-    public static void main(String[] args) {
-        String schemaRegistryUrl = "http://localhost:9081";
-        String bootstrapServers = "localhost:9092";
-        String topic = "cdc_retail_member";
+    public CdcProducer(KafkaTemplate<String, GenericRecord> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.faker = new Faker();
+        this.random = new Random();
+    }
 
-        // Initialize Schema Registry client
-        SchemaRegistryClient schemaRegistry = new CachedSchemaRegistryClient(schemaRegistryUrl, 100);
-
-        // Parse Avro schema
+    @PostConstruct
+    public void simulateCdcEvents() {
         Schema schema = new Schema.Parser().parse(SCHEMA_STRING);
 
-        // Configure Kafka producer
-        Properties props = new Properties();
-        props.put("bootstrap.servers", bootstrapServers);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", KafkaAvroSerializer.class.getName());
-        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-
-        KafkaProducer<String, GenericRecord> producer = new KafkaProducer<>(props);
-        Faker faker = new Faker();
-        Random random = new Random();
-
-        // Event generation loop
         while (true) {
             try {
                 GenericRecord record = new GenericData.Record(schema);
@@ -116,16 +85,7 @@ public class CDCProducer {
                     record.put("address", "");
                 }
 
-                producer.send(new ProducerRecord<>(topic, record.get("member_id").toString(), record), new Callback() {
-                    @Override
-                    public void onCompletion(RecordMetadata metadata, Exception exception) {
-                        if (exception != null) {
-                            exception.printStackTrace();
-                        } else {
-                            System.out.println("Message delivered to topic " + metadata.topic() + " partition " + metadata.partition());
-                        }
-                    }
-                });
+                kafkaTemplate.send(TOPIC, record.get("member_id").toString(), record);
 
                 // Simulate delay between events
                 TimeUnit.SECONDS.sleep(1);
@@ -137,11 +97,9 @@ public class CDCProducer {
                 e.printStackTrace();
             }
         }
-
-        producer.close();
     }
 
-    private static String generateRandomString(int length) {
+    private String generateRandomString(int length) {
         return new Random().ints(48, 122)
                 .filter(i -> (i < 58 || i > 64) && (i < 91 || i > 96))
                 .limit(length)
@@ -149,8 +107,8 @@ public class CDCProducer {
                 .toString();
     }
 
-    private static String randomOperation() {
+    private String randomOperation() {
         String[] operations = {"INSERT", "UPDATE", "DELETE"};
-        return operations[new Random().nextInt(operations.length)];
+        return operations[random.nextInt(operations.length)];
     }
 }
